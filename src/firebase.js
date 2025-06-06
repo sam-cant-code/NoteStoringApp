5d6f74c7ab7323726acef265bc6cfa84e6db422a
sam-cant-code <135305742+sam-cant-code@users.noreply.github.com>
2025-06-06 21:23:33 +0530

    security issue fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from 'firebase/analytics'; // Add this if you need analytics
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBWYUJH_FAY7L2wyuGi-4B7djhmvcr5XE",
-  authDomain: "mynotes-b83e1.firebaseapp.com",
-  projectId: "mynotes-b83e1",
-  storageBucket: "mynotes-b83e1.firebasestorage.app",
-  messagingSenderId: "71489692359",
-  appId: "1:71489692359:web:978c2221c8fd487c04d4e2",
-  measurementId: "G-VZ6MDYZ74B"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
